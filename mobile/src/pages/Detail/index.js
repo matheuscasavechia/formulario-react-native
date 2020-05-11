5d725ec214cbe3fdfcde0f9d828317d4ddfaaf8d
matheuscasavechia <matheus.casavechia1@gmail.com>
2020-05-10 23:03:15 -0300

    Upgrades do mobile

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-
+import {useNavigation} from '@react-navigation/native';
 import styles from './styles';
 
 
 export default function Detail() {
+    const navigation = useNavigation();
+    function navigationBack(){
+        navigation.goBack()
+    }
     return(
         <View style={styles.container}>
             <View style={styles.header}>
 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity style={styles.arrowLeft} onPress={navigationBack}>
                     <Feather name="arrow-left" size={28} color="#3E50B4"/>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.funcionarioName}> Funcionário1 </Text>
-                <Text style={styles.funcionarioSalario}> R$1200,00 </Text>
-                <Text style={styles.funcionarioName}> funcionario1@email.com </Text>
+                
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}> NOME </Text>
+                <Text style={styles.incidentValue}> Funcionário1 </Text>
+                
+                <Text style={styles.incidentProperty}> E-MAIL </Text>
+                <Text style={styles.incidentValue}> funcionario1@email.com </Text>
+                
+                <Text style={styles.incidentProperty}> SALARIO </Text>
+                <Text style={styles.incidentValue}> R$1200,00 </Text>
+                
             </View>
 
+            <View styele={styles.action}>
+                <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <Text style={styles.actionText}>Editar</Text>
+                </TouchableOpacity>
+
+            </View>
         </View>
 
     );
--- mobile/src/pages/Detail/styles.js
+++ mobile/src/pages/Detail/styles.js
@@ -1,5 +1,59 @@
 import {StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    container: {},
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 5, 
+    },
+    arrowLeft:{
+        marginLeft: 330,
+        paddingBottom: 20,
+
+    },
+
+    header:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident:{
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 16,
+    },
+
+    incidentProperty:{
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+        marginTop: 24,
+
+    },
+
+    incidentValue:{
+        marginTop: 8,
+        fontSize: 15,
+        color: '#737380'
+
+    },
+
+    action:{
+        backgroundColor:'#3E50B4',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    actionText: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+
 });
